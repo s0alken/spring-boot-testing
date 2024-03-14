d9840d8b7cb08c89fc34cbef11ac2d4fbd33e91b
s0alken <ropedent@gmail.com>
2024-03-14 01:43:16 -0300

    added JUnit test for getAllEmployees method

--- src/main/java/net/javaguides/springboot/service/EmployeeService.java
+++ src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -2,6 +2,9 @@ package net.javaguides.springboot.service;
 
 import net.javaguides.springboot.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
 }
