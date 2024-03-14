334ba490b8dd8cbdd063b3aa992d2c9a718a4c56
s0alken <ropedent@gmail.com>
2024-03-14 01:50:20 -0300

    added JUnit test for getAllEmployees method (negative scenario)

--- src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
@@ -20,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -100,5 +101,21 @@ public class EmployeeServiceTests {
         // then - verify the output
         Assertions.assertThat(employeeList).isNotNull();
         Assertions.assertThat(employeeList.size()).isEqualTo(2);
+    }
+
+    // JUnit test for getAllEmployees method (negative scenario)
+    @DisplayName("JUnit test for getAllEmployees method (negative scenario)")
+    @Test
+    public void givenEmptyEmployeesList_whenGetAllEmployees_thenReturnEmptyEmployeesList(){
+        // given - precondition or setup
+
+        given(employeeRepository.findAll()).willReturn(Collections.emptyList());
+
+        // when - action or the behavior that we are going to test
+        List<Employee> employeeList = employeeService.getAllEmployees();
+
+        // then - verify the output
+        Assertions.assertThat(employeeList).isEmpty();
+        Assertions.assertThat(employeeList.size()).isEqualTo(0);
     }
 }
