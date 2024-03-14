1e0f284d1570a006f3f5913485b95306d31a5192
s0alken <ropedent@gmail.com>
2024-03-14 00:25:33 -0300

    create EmployeeService

---
+++ src/main/java/net/javaguides/springboot/service/Impl/EmployeeServiceImpl.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot.service.Impl;
+
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import net.javaguides.springboot.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+
+        Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
+
+        if(savedEmployee.isPresent()) {
+            throw new ResourceNotFoundException("Employee already exists with given email: " + employee.getEmail());
+        }
+
+        return employeeRepository.save(employee);
+    }
+}
