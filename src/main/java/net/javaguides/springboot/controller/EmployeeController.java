1cd658ad356d67ee7bc7df0591bcb97d60fa3df3
s0alken <ropedent@gmail.com>
2024-03-17 19:18:37 -0300

    added Junit Controller tests

---
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+}
---
+++ src/test/java/net/javaguides/springboot/controller/EmployeeControllerTests.java
@@ -0,0 +1,86 @@
+package net.javaguides.springboot.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.service.EmployeeService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest
+public class EmployeeControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
+
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Fadare")
+                .email("ramesh@gmail.com")
+                .build();
+
+        BDDMockito.given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+                .willAnswer((invocation)->invocation.getArgument(0));
+
+        // when - action or behavior that we are going to test
+        ResultActions response = mockMvc.perform(post("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+
+        // then - verify the result or output using assert statements
+        response.andDo(MockMvcResultHandlers.print()).
+                andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName",
+                        CoreMatchers.is(employee.getFirstName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName",
+                        CoreMatchers.is(employee.getLastName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email",
+                        CoreMatchers.is(employee.getEmail())));
+
+    }
+
+    // JUnit test for Get All employees REST API
+    @Test
+    public void givenListOfEmployees_whenGetAllEmployees_thenReturnEmployeesList() throws Exception {
+
+        // given - precondition or setup
+        List<Employee> listOfEmployees = new ArrayList<>();
+        listOfEmployees.add(Employee.builder().firstName("Ramesh").lastName("Fadare").email("ramesh@gmail.com").build());
+        listOfEmployees.add(Employee.builder().firstName("Tony").lastName("Montana").email("hannah@gmail.com").build());
+
+        BDDMockito.given(employeeService.getAllEmployees()).willReturn(listOfEmployees);
+
+        // when - action or behavior that we are going to test
+        ResultActions response = mockMvc.perform(get("/api/employees"));
+
+        // then - verify the result or output using assert statements
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(2)));
+
+    }
+}
